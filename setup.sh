b238f66c9a777bb039962ebbba9acbf726f6e3ef
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2020-10-17 02:27:37 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,44 @@
-setup.sh
+#!/bin/bash
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v"
+echo
+pkg install python
+clear
+pkg install filet
+clear
+pkg install ruby -y
+clear
+gem install lolcat
+clear
+figlet YayanXD | lolcat
+sleep 2
+clear
+figlet YayanXD | lolcat
+sleep 2
+clear
+echo
+echo "██    ██ ██   ██ ███    ██ ███████ " | lolcat
+echo " ██  ██  ██   ██ ████   ██    ███  " | lolcat
+echo "  ████   ███████ ██ ██  ██   ███   " | lolcat
+echo "   ██         ██ ██  ██ ██  ███    " | lolcat
+echo "   ██         ██ ██   ████ ███████ " | lolcat
+echo
+clear
+echo
+echo "╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮" | lolcat
+echo "╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯" | lolcat
+echo "┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈" | lolcat
+echo "┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈" | lolcat
+echo "╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮" | lolcat
+echo "╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯" | lolcat
+echo
+clear
+pip2 install -r requirements.txt
+clear
+pip2 install mechanize
+clear
+pip2 install requests
+clear
+python2 run.py
