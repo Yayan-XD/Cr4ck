155bc21de509f1d336a49de8cd34bc5cffdb1c76
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2020-11-09 11:19:24 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,18 +8,46 @@ pip2 install requests
 clear
 pip2 install tqdm
 clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v"
+echo
 pip2 install mechanize
 clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v"
+echo
 pip install requests
 clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v"
+echo
 pip install mechanize
 clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v"
+echo
 pip install requests bs4
 clear
-pkg install filet
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v"
+echo
+pkg install figlet
 clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v"
+echo
 pkg install ruby -y
 clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v"
+echo
 gem install lolcat
 clear
 figlet YayanXD | lolcat
