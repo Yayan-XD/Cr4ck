80b5680c0b3ad0ac759bb3c75f09413fb54a67c5
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2021-03-04 06:03:10 +0700

    Update Cr4ck.py

--- Cr4ck.py
+++ Cr4ck.py
@@ -48,7 +48,7 @@ def yayanxd():
     if yayan == "": 
        os.system("python Cr4ck.py")
     else:
-       sys.exit("\n\033[1;97m [\033[1;91m•\033[1;97] \033[1;91mGood Byee :')")
+       sys.exit("\n\033[1;97m [\033[1;91m•\033[1;97m] \033[1;91mGood Byee :')")
 def moch_yayan():
     time.sleep(0.1)
     print("\033[97m [\033[96m01\033[97m] Start  Cr4ck ID")
