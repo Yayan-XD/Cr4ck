d4a7b5196c4130a45cfe5d0369b7266ba7a7dba2
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2021-03-10 19:49:47 +0700

    Update Cr4ck.py

--- Cr4ck.py
+++ Cr4ck.py
@@ -351,7 +351,7 @@ def moch_yayan():
          os.system('xdg-open https://www.facebook.com/groups/1683226775285117')
          yayanxd()
     elif yayan == "4" or yayan =="04":
-         os.system('xdg-open https://www.facebook.com/groups/453688872336137')
+         os.system('xdg-open https://www.facebook.com/groups/868734063220514')
          yayanxd()
     elif yayan == "5" or yayan =="05":
          print("\n\n\x1b[1;97m      [ \x1b[1;92mPlease Wait While Updating The Tools \x1b[1;97m]\n")
