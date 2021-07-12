851b929e7234c896f21c68ced3ffca92edb622dc
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2021-07-13 01:27:04 +0700

    Update Cr4ck.py

--- Cr4ck.py
+++ Cr4ck.py
@@ -348,7 +348,7 @@ def moch_yayan():
          os.system("xdg-open https://youtu.be/72zvkSbVPOI") 
          yayanxd()
     elif yayan == "3" or yayan =="03":
-         os.system('xdg-open https://www.facebook.com/groups/271952186154338')
+         os.system('xdg-open https://www.facebook.com/groups/1592269051080491')
          yayanxd()
     elif yayan == "4" or yayan =="04":
          os.system('xdg-open https://www.facebook.com/groups/310605552656196')
